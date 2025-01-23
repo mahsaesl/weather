faa8b60dffde73c2d549b9a4a1cc6bc5533de320
mahsaesl <mahsaaaesl@gmail.com>
2025-01-23 09:18:44 +0330

    added search and api

--- source/script.js
+++ source/script.js
@@ -1,9 +1,45 @@
+
+
+
+function updateweather(response){
+  console.log(response);
+  let correctTemperature=document.querySelector("#temperature");
+  correctTemperature.innerHTML= Math.round (response.data.temperature.current) ;
+  let city = document.querySelector("#city");
+  city.innerHTML =response.data.city;
+  
+
+}
+
+
+
+
+
+
+
+
+function searchcity(city){
+  let api = "c2526553620odab6dft0efe428b8b025";
+  let apiUrl=`https:api.shecodes.io/weather/v1/current?query=${city}&key=${api}`
+  console.log(apiUrl);
+  axios.get(apiUrl).then(updateweather);
+}
+
+
+
+
+
+
+
 function runTheSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector(".search-form-input");
-  let city = document.querySelector("#city");
-  city.innerHTML = searchInput.value;
+  
+  searchcity(searchInput.value)
 }
 
 let search = document.querySelector(".search-form");
 search.addEventListener("submit", runTheSearch);
+
+
+searchcity("tehran");
