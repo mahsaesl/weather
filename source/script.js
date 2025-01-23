d5d1419033efbdaea72019770f42b6964a87fe8a
mahsaesl <mahsaaaesl@gmail.com>
2025-01-23 08:39:06 +0330

    added axios

--- source/script.js
+++ source/script.js
@@ -1,9 +1,9 @@
-function runTheSearch() {
+function runTheSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector(".search-form-input");
   let city = document.querySelector("#city");
   city.innerHTML = searchInput.value;
 }
 
-let search=document.querySelector("#search-input");
+let search = document.querySelector(".search-form");
 search.addEventListener("submit", runTheSearch);
